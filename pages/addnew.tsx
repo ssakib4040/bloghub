1594d95105d5f80c657737072dd99ba552cb8467
Sadman Sakib <ssakib4040@gmail.com>
2022-11-21 22:14:59 +0600

    New Post page added

--- pages/addnew.tsx
+++ pages/addnew.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { Button, Container } from "react-bootstrap";
 
 export default function Addnew() {
-  return <div>A</div>;
+  return (
+    <Container>
+      <h1>Add new post</h1>
+      <label htmlFor="">Title</label>
+      <input
+        type="text"
+        placeholder="Enter your title"
+        className="form-control mb-3"
+      />
+
+      <label htmlFor="">Post Content</label>
+      <textarea
+        name=""
+        id=""
+        cols={10}
+        className="form-control mb-3"
+        placeholder="Enter your post content"
+      ></textarea>
+      <div>
+        <Button variant="dark" className="mb-3">
+          Add new post
+        </Button>
+      </div>
+    </Container>
+  );
 }
