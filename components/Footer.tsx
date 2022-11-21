131e4c551c651167109286930fefaa9faa6b9591
Sadman Sakib <ssakib4040@gmail.com>
2022-11-21 19:30:18 +0600

    Footer added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-dark text-white px-5 py-4 text-center">
+      &copy; Copyright 2022 All Rights reserved
+    </div>
+  );
 }
