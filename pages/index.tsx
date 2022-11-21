a8be11aba4357f062eb508025e3e3c32e51c2641
Sadman Sakib <ssakib4040@gmail.com>
2022-11-22 02:18:27 +0600

    Post fetching logic added in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,45 @@ import Container from "react-bootstrap/Container";
 import { Row } from "react-bootstrap";
 import { useRouter } from "next/router";
 
-export default function Home() {
+export default function Home(props: any) {
   const router = useRouter();
+
   return (
     <Container className="pt-3">
       <Row className="">
         {/* Posts  */}
-        <div
-          className="border cursor-pointer p-2 mb-3"
-          onClick={() => {
-            router.push("/posts/123");
-          }}
-        >
-          <h4>Post title number 1</h4>
-          <p className="mb-0">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            tempora minus molestias expedita vitae recusandae cum, ad beatae
-            quis delectus!
-          </p>
-        </div>
+
+        {props.data.map((post: any, index: string) => {
+          return (
+            <div
+              key={index}
+              className="border cursor-pointer p-2 mb-3"
+              onClick={() => {
+                router.push("/posts/123");
+              }}
+            >
+              <h4>Post title number 1</h4>
+              <p className="mb-0">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+                tempora minus molestias expedita vitae recusandae cum, ad beatae
+                quis delectus!
+              </p>
+            </div>
+          );
+        })}
       </Row>
     </Container>
   );
 }
 
-// export async function getStaticProps() {
-//   return {
-//     props: {},
-//   };
-// }
+import axios from "axios";
+
+export async function getStaticProps() {
+  const { data } = await axios.get(`${process.env.WEBSITE_URI}/api/posts`);
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
